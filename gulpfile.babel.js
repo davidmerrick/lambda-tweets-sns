2d691938c7923595ae3f1d8ad64820e4e871f10d
david.merrick <david.merrick@jivesoftware.com>
2017-06-05 15:39:25 -0700

    Added babel plugin for handling async

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,11 +12,7 @@ gulp.task('clean', function() {
 
 gulp.task('js', function() {
     return gulp.src('src/**/*')
-        .pipe(
-            babel({
-                presets: ['es2015']
-            })
-        )
+        .pipe(babel())
         .pipe(gulp.dest('dist/'));
 });
 
